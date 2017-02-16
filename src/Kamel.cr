cf33d5d8ead4752a8c9e4fb3826fc7e23f87f010
rishavs <rishav.sharan@live.com>
2017-02-17 03:20:43 +0530

    Initiated basic db connection

--- src/Kamel.cr
+++ src/Kamel.cr
@@ -1,6 +1,27 @@
 require "kemal"
 require "ecr"
+require "db"
+require "pg"
 
+
+
+before_get "/posts" do |env|
+	DB.open "postgres://wguyczml:Z1CKsVDby2pp154JcMm_eSnev6v_ayFR@fizzy-cherry.db.elephantsql.com:5432/wguyczml" do |db|
+		db.query "select title, content from posts" do |result|
+		    result.each do
+			    title = result.read(String)
+			    content = result.read(String)
+			    puts "#{title}" 
+			    puts "#{content}"
+			end
+		end
+	end
+end
+
+get "/posts" do 
+	#{title}
+	#{content}
+end
 
 #----------------------
 # Redirect browser
@@ -16,16 +37,16 @@ end
 
 #----------------------
 get "/error" do |env|
-  env.response.status_code = 403
+    env.response.status_code = 403
 end
 
 
 error 404 do
-  "This is a customized 404 page."
+    "This is a customized 404 page."
 end
 
 error 403 do
-  "Access Forbidden!"
+    "Access Forbidden!"
 end
 
 #----------------------
