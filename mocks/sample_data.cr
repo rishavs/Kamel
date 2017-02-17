6cc8837b4ece2f7fa9fa29c4308ed584498ca5e8
rishavs <rishav.sharan@live.com>
2017-02-17 22:20:38 +0530

    Sample Data for Posts generated. cleaned the scipt

--- mocks/sample_data.cr
+++ mocks/sample_data.cr
@@ -28,18 +28,18 @@ begin
         puts "Table doesn't exists. creating table..."
 
         db.exec "CREATE TABLE posts(
-            id serial primary key,
-            title character varying(255),
-            is_link boolean default true,
-            link character varying(255),
-            content text,
+            id              serial primary key,
+            title           character varying(255),
+            is_link         boolean default true,
+            link            character varying(255),
+            content         text,
             author_username character varying(255),
-            is_viewable boolean default false,
-            is_approved boolean default false,
-            is_deleted boolean default false,
-            created_at timestamp default current_timestamp,
-            updated_at timestamp,
-            last_read_at timestamp
+            is_viewable     boolean default false,
+            is_approved     boolean default false,
+            is_deleted      boolean default false,
+            created_at      timestamp default current_timestamp,
+            updated_at      timestamp,
+            last_read_at    timestamp
         );"
         puts "Table created. Populating with sample data. Datasize = #{size}"
 
@@ -51,65 +51,20 @@ begin
 
     (1..size).each do |i|
         db.exec "INSERT INTO posts (title, is_link, link, content, author_username) VALUES
-            ('#{Faker::Lorem.sentence}', 'true', '#{Faker::Internet.url}', '#{Faker::Lorem.paragraph(50, true, 50)}', '#{Faker::Internet.user_name}');"
+            ('#{Faker::Lorem.sentence(3, true, 4)}', 'true', '#{Faker::Internet.url}', '#{Faker::Lorem.paragraph(50, true, 50)}', '#{Faker::Internet.user_name}');"
+
         if i % 20 == 0 
             puts "20 records inserted..."
         end
+
     end
 
     puts "Sample data Populated!"
 
     table_size = db.scalar "select count(*) from posts;"
+
     puts "Current table size is #{table_size} rows"
 
-
-
-
-    # db.query "select name, age from contacts order by age desc" do |rs|
-    #     rs.each do
-    #         # ... perform for each row in the ResultSet
-    #     end
-    # end
 ensure
   db.close
 end
-
-
-
-# # Creates connection with the default postgres db
-# conn = PG.connect("#{PG_PATH}/postgres")
-
-# database_exists? = conn.exec(%{
-#     SELECT CAST(1 AS integer)
-#     FROM pg_database
-#     WHERE datname=$1
-# }, [DB_NAME]).to_hash.empty? ? false : true
-
-# if !database_exists?
-#     # Creates the notes_db database with UTF8 encoding and close the connection
-#     puts "Creating database: #{DB_NAME}..."
-#     conn.exec("CREATE DATABASE #{DB_NAME} ENCODING 'UTF8';")
-#     conn.close
-
-#     # Creates connection with the newly created db
-#     puts "Connecting database: #{DB_NAME}..."
-#     conn = PG.connect("#{PG_PATH}/#{DB_NAME}")
-
-#     # Creates the notes table in the newly created database
-#     puts "Creating notes table in #{DB_NAME}..."
-#     conn.exec(%{
-#         CREATE TABLE notes (
-#             id                    SERIAL PRIMARY KEY,
-#             title             TEXT NOT NULL,
-#             content         TEXT NOT NULL,
-#             created_at    TIMESTAMP WITH TIME ZONE NOT NULL,
-#             updated_at    TIMESTAMP WITH TIME ZONE NOT NULL
-#         );
-#     })
-#     puts "Process finished succesfully"
-# else
-#     puts "The database #{DB_NAME} already exists!!"
-# end
-
-# puts "Closing connection..."
-# conn.close
