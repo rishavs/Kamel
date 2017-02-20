9ee8c7875b1172b7b402747888a85932790db6c1
rishavs <rishav.sharan@live.com>
2017-02-20 21:12:41 +0530

    in progress save

--- src/Kamel/models/posts.cr
+++ src/Kamel/models/posts.cr
@@ -1,15 +1,21 @@
-class Post < Crecto::Model
+require "crecto"
 
-  	schema "posts" do
-        field :title, 		String,
-        # field :is_link, 	Bool,
-        # field :link, 		String,
-        field :content,		String,
-        # field :author_name,	String,
-        # field :is_viewable, Bool,
-        # field :is_approved, Bool,
-        field :is_deleted,  Bool,
-	end
-	
-	validate_required [:title]
+module Kamel
+
+    class Post < Crecto::Model
+
+      	schema "posts" do
+            field :title, 		String,
+            # field :is_link, 	Bool,
+            # field :link, 		String,
+            field :content,		String,
+            # field :author_name,	String,
+            # field :is_viewable, Bool,
+            # field :is_approved, Bool,
+            field :is_deleted,  Bool
+    	end
+    	
+    	validate_required [:title]
+    end
+
 end
--- src/Kamel/routes.cr
+++ src/Kamel/routes.cr
@@ -1,32 +1,78 @@
+require "crecto"
+
 module Kamel
+
+    class Post < Crecto::Model
+
+      	schema "posts" do
+            field :title, 		String,
+            # field :is_link, 	Bool,
+            # field :link, 		String,
+            field :content,		String,
+            # field :author_name,	String,
+            # field :is_viewable, Bool,
+            # field :is_approved, Bool,
+            field :is_deleted,  Bool
+    	end
+    	
+    	validate_required [:title]
+    end
 
 	get "/p/:id" do 
 		#{title}
 		#{content}
+		# TODO
 	end
 
-	get "/p/new" do |env|
+	get "/p/new" do 
 	  	render "src/Kamel/views/new_post.ecr", "src/Kamel/views/layouts/base.ecr"
 	end
 
 	post "/p/create" do |env|
- 		field = env.params.body["post_title"]
- 		puts field
+
+
+		data = Post.new
+
+ 		data.title = env.params.body["post_title"]
+ 		data.content = env.params.body["post_content"]
+ 		
+ 		changeset = Crecto::Repo.insert(data)
+		puts changeset.valid? # false
+		puts changeset.errors # {:field => "name", :message => "is invalid"}
+		puts changeset.changes # {:name => "123", :age => 123}
+
+ 		puts data
+ 		puts data.title
+
+ 		puts "jhinga la la"
 	end
 
-	#----------------------
+
+
+	#---------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
 	# Redirect browser
 	get "/foo" do |env|
 	  	# important stuff like clearing session etc.
 	 	env.redirect "/bar" # redirect to /bar page
 	end
-
-	# Redirect browser
 	get "/bar" do 
 		"type foo get bar"
 	end
 
-	#----------------------
+
 	get "/error" do |env|
 	    env.response.status_code = 403
 	end
@@ -40,13 +86,12 @@ module Kamel
 	    "Access Forbidden!"
 	end
 
-	#----------------------
+
 	get "/:name" do |env|
 	  	name = env.params.url["name"]
 	  	render "src/Kamel/views/hello.ecr", "src/Kamel/views/layouts/base.ecr"
 	end
 
-	#----------------------
 	get "/" do
 		"Hello World!"
 	end
