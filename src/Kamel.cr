f5c379743c3ba8a6430ef57cf7ebb177a9c0529f
rishavs <rishav.sharan@live.com>
2017-02-24 02:52:02 +0530

    missed checking in

--- src/Kamel.cr
+++ src/Kamel.cr
@@ -1,19 +1,19 @@
 require "kemal"
 require "ecr"
 require "dotenv"
-require "crecto"
-require "db"
 require "pg"
+require "crecto"
+
 
 require "./Kamel/models/*"
 require "./Kamel/controllers/*"
 require "./Kamel/*"
 
-module Kamel
+
 	
-	# Dotenv.load
+# Dotenv.load
 	
 	# Run Kamel
-	Kemal.run
+Kemal.run
 
-end
+
